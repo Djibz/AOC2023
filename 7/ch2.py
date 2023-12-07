099531951c231f18119da96ae2f21e8c8c080c3a
Erwan <erwan.brunelliere@gmail.com>
2023-12-07 08:39:42 +0100

    day 7

---
+++ 7/ch2.py
@@ -0,0 +1,88 @@
+import re
+
+cards = '1AKQT98765432J'
+
+hands_values = dict()
+hands_values[0] = []
+hands_values[1] = []
+hands_values[2] = []
+hands_values[3] = []
+hands_values[4] = []
+hands_values[5] = []
+hands_values[6] = []
+
+nb_lines = 0
+with open('./7/input.txt', 'r') as input:
+
+  print("Part 1")
+
+  for line in input:
+    nb_lines += 1
+
+    (hand, bid) = line.split()
+    
+    vwin = 0
+    for card in cards[:-1]:
+      new_v = 0
+      
+      matches = re.findall(fr'{card}|J', hand)
+      
+      if len(matches) == 5:
+        new_v = 6
+
+      if len(matches) == 4:
+        new_v = 5
+
+      if len(matches) == 3:
+
+        if vwin == 1 and ('J' not in hand or 'J' not in matches): new_v = 4
+        elif vwin == 3:
+          if matches.count('J') == 2: new_v = 3
+          elif 'J' in matches: new_v = 4
+          else: new_v = 3
+        else: new_v = 3
+
+      if len(matches) == 2:
+        
+        if vwin == 3:
+          if 'J' not in hand or 'J' not in matches: new_v = 4
+          else: new_v = 3
+        elif vwin == 1 and ('J' not in hand or 'J' not in matches): new_v = 2
+        else: new_v = 1
+
+      if new_v > vwin:
+        vwin = new_v
+
+    hands_values[vwin].append((hand, bid))
+
+print(hands_values)
+
+def hand_v(hand):
+  total = 0
+  for i in range(5):
+    total += (len(cards) - cards.index(hand[i])) * (6 - (i+1) * 5)
+
+  return total
+
+#for v, k in hands_values.items():
+  k.sort(key= lambda x: hand_v(x[0]), reverse=False)
+
+total = 0
+count = 1
+for k, v in hands_values.items():
+
+  while len(v) != 0:
+    smaller = ('11111', '0')
+    for i in range(0, 5):
+      for h in v:
+        if h[0][0:i] == smaller[0][0:i]:
+          if cards.index(h[0][i]) > cards.index(smaller[0][i]):
+            smaller = h
+
+    total += count * int(smaller[1])
+    count += 1
+    v.remove(smaller)
+    if 'J' in smaller[0]:
+      print(k, smaller[0])
+
+print(total)
