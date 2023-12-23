6016f1e344c82159468c44dcf9b987265b11a51c
Erwan <erwan.brunelliere@gmail.com>
2023-12-23 19:27:13 +0100

    nul

--- 22/challenge.py
+++ 22/challenge.py
@@ -5,45 +5,37 @@ with open('./22/example.txt', 'r') as input:
     b, e = line[:-1].split('~')
     bricks.append(([int(x) for x in b.split(',')], [int(x) for x in e.split(',')]))
 
-print(bricks)
+def collide(a, b):
+  for xa in range(a[0][0], a[0][0] - a[0][0] + 1):
+    for ya in range(a[0][1], a[1][1] - a[0][1] + 1):
+      for za in range(a[0][2], a[1][2] - a[0][2] + 1):
+        for xb in range(b[0][0], b[0][0] - b[0][0] + 1):
+          for yb in range(b[0][1], b[1][1] - b[0][1] + 1):
+            for zb in range(b[0][2], b[1][2] - b[0][2] + 1):
+              if xa == xb and ya == yb and za == zb:
+                return True
+  return False
 
-def share_plan(a, b):
-  for i in range(len(a[0])):
-    if a[0][i] == a[1][i] == b[0][i] == b[1][i]:
-      return i
-  return -1
-
-def collide(a, b) -> bool:
-  sp = share_plan(a, b)
-  if sp == -1:
-    return False
-  
-  if sp == 0:
-    pa = (a[1], a[2])
-    pb = (b[1], b[2])
-
-  if sp == 1:
-    pa = (a[0], a[2])
-    pb = (b[0], b[2])
-
-  if sp == 2:
-    pa = (a[0], a[1])
-    pb = (b[0], b[1])
-
-  if pa[0] != pa[1] and pb[0] != pb[1]:
-    if pa[1] != pb[1]:
-      return False
-    return
-  
-  return True
-
-
-def collide_any(brick):
+def collide_any(brick, bricks):
   for b in bricks:
     if collide(brick, b):
       return True
   return False
   
 for b in bricks:
-  assert collide_any(b) == False
+  assert collide_any(b, bricks) == False
+
+def fall_everything(bricks):
+  changes = False
+  while True:
+    for b in bricks:
+      if b[0][2] == 0:
+        break
+      bricks.remove(b)
+      new_brick = ((b[0][0], b[0][1], b[0][2] - 1), (b[1][0], b[1][1], b[1][2] - 1))
+      if collide_any(new_brick, bricks):
+        bricks.append(b)
+        break
+      
+
   
